59e7cd173f01314c634421478a3aba93851c235c
Nicolas Vargas <nhvargas@uniboyaca.edu.co>
2026-05-08 22:55:54 -0500

    cambios

--- Base de datos/Conexion.php
+++ Base de datos/Conexion.php
@@ -11,5 +11,17 @@ if($conn->connect_error){
     die("Error de conexión");
 }
 
-echo "Conexión exitosa";
+$sql = "SELECT * FROM sprints";
+
+$resultado = $conn->query($sql);
+
+echo "<h1>Listado de Sprints</h1>";
+
+while($fila = $resultado->fetch_assoc()){
+
+    echo "ID: " . $fila['id'] . "<br>";
+    echo "Nombre: " . $fila['nombre'] . "<br>";
+    echo "Inicio: " . $fila['fecha_inicio'] . "<br>";
+    echo "Fin: " . $fila['fecha_fin'] . "<br><hr>";
+}
 ?>
