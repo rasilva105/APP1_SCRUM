1932ed75ef956bab43a95ed530981c8e4c889771
Nicolas Vargas <nhvargas@uniboyaca.edu.co>
2026-05-09 01:05:52 -0500

    cambios

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
+<?php
+define('BASE_URL', '/APP1_SCRUM/');
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>APP1 SCRUM</title>
 
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/style.css">
 </head>
 <body>
 
