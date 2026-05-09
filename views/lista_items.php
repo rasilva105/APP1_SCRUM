8d7f6efb9314aeb051b224abc54ffbaefc10d7c3
rasilva105 <rssilva@uniboyaca.edu.co>
2026-05-09 00:33:12 -0500

    FINAL2

--- views/lista_items.php
+++ views/lista_items.php
@@ -26,28 +26,3 @@ if (!isset($items)) {
 </tr>
 
 <?php } ?>
-
-</table>
-```php
-<h2>Lista Items</h2>
-
-<table>
-<tr>
-    <th>ID</th>
-    <th>Sprint</th>
-    <th>Categoría</th>
-    <th>Descripción</th>
-</tr>
-
-<?php while($item = mysqli_fetch_assoc($items)) { ?>
-
-<tr>
-    <td><?= $item['id'] ?></td>
-    <td><?= $item['sprint_nombre'] ?></td>
-    <td><?= $item['categoria'] ?></td>
-    <td><?= $item['descripcion'] ?></td>
-</tr>
-
-<?php } ?>
-
-</table>
