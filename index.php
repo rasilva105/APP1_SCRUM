2248e67aa954b634bb3c7ead6b43da780e48b5f3
Nicolas Vargas <nhvargas@uniboyaca.edu.co>
2026-05-09 00:57:48 -0500

    cambios

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 require_once 'controllers/retrospectiva_controller.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>APP1 SCRUM</title>
+
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+
+</body>
+</html>
